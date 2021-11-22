a3da93dbae83edee087708b95249e3ae9b56c5d1
michaelilnitski <michael.ilnitski@smart-digital.de>
2021-11-22 20:50:40 +0100

    status with js added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("backgroundBtn").addEventListener("click",function() {
+    console.log("button clicked")
+    document.getElementById("pastaTitel").style.color="red"
+    /* document.getElementById("pastaTitel").style.fontSize="12px" */
+    document.getElementById("backgroundColor").style.backgroundColor="red"
+} )
